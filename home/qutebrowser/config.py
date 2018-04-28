127476f99a3cbdef66d5fe58742b3450ebfb9c36
sunderkeenin <sunderkeenin@gmail.com>
2018-04-27 19:55:32 -0500

    switching to wayland and sway???

--- home/qutebrowser/config.py
+++ home/qutebrowser/config.py
@@ -64,87 +64,87 @@ c.colors.webpage.bg = 'white'
 # Default monospace fonts. Whenever "monospace" is used in a font
 # setting, it's replaced with the fonts listed here.
 # Type: Font
-c.fonts.monospace = 'D2Coding'
+c.fonts.monospace = 'Luculent'
 
 # Font used in the completion widget.
 # Type: Font
-c.fonts.completion.entry = '10pt D2Coding'
+c.fonts.completion.entry = '10pt Luculent'
 
 # Font used in the completion categories.
 # Type: Font
-c.fonts.completion.category = 'bold 10pt D2Coding'
+c.fonts.completion.category = 'bold 10pt Luculent'
 
 # Font used for the debugging console.
 # Type: QtFont
-c.fonts.debug_console = '10pt D2Coding'
+c.fonts.debug_console = '10pt Luculent'
 
 # Font used for the downloadbar.
 # Type: Font
-c.fonts.downloads = '10pt D2Coding'
+c.fonts.downloads = '10pt Luculent'
 
 # Font used for the hints.
 # Type: Font
-c.fonts.hints = 'bold 10pt D2Coding'
+c.fonts.hints = 'bold 10pt Luculent'
 
 # Font used in the keyhint widget.
 # Type: Font
-c.fonts.keyhint = '10pt D2Coding'
+c.fonts.keyhint = '10pt Luculent'
 
 # Font used for error messages.
 # Type: Font
-c.fonts.messages.error = '10pt D2Coding'
+c.fonts.messages.error = '10pt Luculent'
 
 # Font used for info messages.
 # Type: Font
-c.fonts.messages.info = '10pt D2Coding'
+c.fonts.messages.info = '10pt Luculent'
 
 # Font used for warning messages.
 # Type: Font
-c.fonts.messages.warning = '10pt D2Coding'
+c.fonts.messages.warning = '10pt Luculent'
 
 # Font used for prompts.
 # Type: Font
-c.fonts.prompts = '10pt D2Coding'
+c.fonts.prompts = '10pt Luculent'
 
 # Font used in the statusbar.
 # Type: Font
-c.fonts.statusbar = '10pt D2Coding'
+c.fonts.statusbar = '10pt Luculent'
 
 # Font used in the tab bar.
 # Type: QtFont
-c.fonts.tabs = '10pt D2Coding'
+c.fonts.tabs = '10pt Luculent'
 
 # Font family for standard fonts.
 # Type: FontFamily
-c.fonts.web.family.standard = 'D2Coding'
+c.fonts.web.family.standard = 'Luculent'
 
 # Font family for fixed fonts.
 # Type: FontFamily
-c.fonts.web.family.fixed = 'D2Coding'
+c.fonts.web.family.fixed = 'Luculent'
 
 # Font family for serif fonts.
 # Type: FontFamily
-c.fonts.web.family.serif = 'D2Coding'
+c.fonts.web.family.serif = 'Luculent'
 
 # Font family for sans-serif fonts.
 # Type: FontFamily
-c.fonts.web.family.sans_serif = 'D2Coding'
+c.fonts.web.family.sans_serif = 'Luculent'
 
 # Font family for cursive fonts.
 # Type: FontFamily
-c.fonts.web.family.cursive = 'D2Coding'
+c.fonts.web.family.cursive = 'Luculent'
 
 # Font family for fantasy fonts.
 # Type: FontFamily
-c.fonts.web.family.fantasy = 'D2Coding'
+c.fonts.web.family.fantasy = 'Luculent'
 
 # Default font size (in pixels) for regular text.
 # Type: Int
-c.fonts.web.size.default = 15
+c.fonts.web.size.default = 14
 
 # Default font size (in pixels) for fixed-pitch text.
 # Type: Int
-c.fonts.web.size.default_fixed = 14
+c.fonts.web.size.default_fixed = 10
 
 # Hard minimum font size (in pixels).
 # Type: Int
